a05fff06737f68bcab27bd310016d42d6be8d9ca
52black <3274235903@qq.com>
2023-03-11 05:09:19 +0800

    1

---
+++ preload.js
@@ -0,0 +1,12 @@
+const {
+  contextBridge,
+  ipcRenderer
+} = require("electron");
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+  "exit", (()=>{
+    ipcRenderer.send('win-close','close')
+  })
+);
